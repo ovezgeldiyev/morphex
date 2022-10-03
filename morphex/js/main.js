bd8da2e9c094c828374151fe11bb0f533fccd281
ovezgeldiyev <ovezgeldiyevagabay2000@gmail.com>
2022-10-03 22:40:06 +0500

    asdasd

--- morphex/js/main.js
+++ morphex/js/main.js
@@ -10,12 +10,15 @@ var progress = document.getElementById("progress");
 var lock = document.getElementById("lock");
 var lockInner = document.getElementById("lockInner");
 var lockBtn = document.getElementById("lockBtn");
+
 menuBtn.onclick = function () {
   menu.classList.toggle("active");
   menuBtn.classList.toggle("active");
   body.classList.toggle("active");
-  popup.classList.remove("active");
-  popupOuter.classList.remove("active");
+  if (popup) {
+    popup.classList.remove("active");
+    popupOuter.classList.remove("active");
+  }
 };
 window.onclick = function (event) {
   if (event.target == menu) {
@@ -23,7 +26,7 @@ window.onclick = function (event) {
     menuBtn.classList.remove("active");
     body.classList.remove("active");
   }
-  if (event.target == popupOuter) {
+  if (event.target == popupOuter && popupOuter) {
     popup.classList.remove("active");
     popupOuter.classList.remove("active");
     body.classList.remove("active");
@@ -34,19 +37,31 @@ window.onclick = function (event) {
     body.classList.remove("active");
   }
 };
-popupBtn.onclick = function () {
-  popup.classList.toggle("active");
-  popupOuter.classList.toggle("active");
-  body.classList.toggle("active");
-};
-checkbox.onclick = function () {
-  progress.classList.toggle("active");
-};
+
+if (popupBtn) {
+  popupBtn.onclick = function () {
+    popup.classList.toggle("active");
+    popupOuter.classList.toggle("active");
+    body.classList.toggle("active");
+  };
+}
+
+if (checkbox && progress) {
+  checkbox.onclick = function (e) {
+    if (e.target.checked) {
+      progress.style = "width: 100%;";
+    } else {
+      progress.style = "width: 65%;";
+    }
+    // progress.classList.toggle("active");
+  };
+}
 
 // menu end
-
-lockBtn.onclick = function () {
-  lock.classList.toggle("active");
-  lockInner.classList.toggle("active");
-  body.classList.toggle("active");
-};
+if (lockBtn) {
+  lockBtn.onclick = function () {
+    lock.classList.toggle("active");
+    lockInner.classList.toggle("active");
+    body.classList.toggle("active");
+  };
+}
